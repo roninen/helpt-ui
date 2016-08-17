6886a044ebd5fa5dda9bfcbca66f0f94bcf09fba
Timo Tuominen <tituomin@gmail.com>
2016-08-17 14:56:05 +0300

    Initial commit.

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import 'core-js/fn/object/assign';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/Main';
+import 'bootstrap-sass';
+
+// Render the main component into the dom
+ReactDOM.render(<App />, document.getElementById('app'));
