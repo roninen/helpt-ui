3beaa1a86c8e51db266471dab4ae0e498f4afcd3
Timo Tuominen <tituomin@gmail.com>
2016-08-26 16:37:45 +0300

    Add missing file.

---
+++ src/util/data.js
@@ -0,0 +1,11 @@
+import _ from 'lodash';
+
+export function findEntryForTask(entries, userId, task, date, options={findDeleted: false}) {
+  return _.find(entries, (entry) => {
+    return (entry.user == userId &&
+            entry.task == task.origin_id &&
+            entry.workspace == task.workspace &&
+            entry.date == date &&
+            (entry.state != 'deleted' || (options.findDeleted === true)));
+  });
+}
