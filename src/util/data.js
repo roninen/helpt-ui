29ee70562666c10823b2cec2eb7c3071938afbae
Timo Tuominen <tituomin@gmail.com>
2016-09-07 16:17:10 +0300

    Selectorless data filtering and expansion.

--- src/util/data.js
+++ src/util/data.js
@@ -9,3 +9,35 @@ export function findEntryForTask(entries, userId, task, date, options={findDelet
             (entry.state != 'deleted' || (options.findDeleted === true)));
   });
 }
+
+function getResourceItems(state, resourceType) {
+  return state.data[resourceType];
+}
+
+function expandKey(state, unexpandedValue, key) {
+  const multiple = Array.isArray(unexpandedValue);
+  const subItemIds = multiple ? unexpandedValue : [unexpandedValue];
+  const expanded = _.filter(
+    getResourceItems(state, key),
+    subitem => subItemIds.includes(subitem.id));
+  if (subItemIds.length > expanded.length) {
+    // If the related data hasn't been fetched
+    // yet, don't expand to undefined.
+    return unexpandedValue;
+  }
+  return multiple ? expanded : expanded[0];
+}
+
+function itemExpander(state, keysToExpand) {
+  return (item) => {
+    const expandedSubItems = _.fromPairs(
+      _.map(
+        keysToExpand,
+        key => [key, expandKey(state, item[key], key)]));
+    return item.merge(expandedSubItems);
+  };
+}
+
+export function expandItems(state, items, keysToExpand) {
+  return _.map(items, itemExpander(state, keysToExpand));
+}
