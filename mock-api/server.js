a9838e483a2f3ac1b590bc751064f77627878926
Timo Tuominen <tituomin@gmail.com>
2017-06-07 14:25:44 +0300

    Make mock api compatible with real api again

---
+++ mock-api/server.js
@@ -0,0 +1,23 @@
+// server.js
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('mock-api/db.json')
+const middlewares = jsonServer.defaults()
+
+router.render = (req, res) => {
+  const match = req.url.match(/\/([a-z]+)\//);
+  if (match) {
+    const resource = match[1];
+    if (res.locals.data) {
+        res.jsonp({
+            [resource]: res.locals.data
+        });
+    }
+  }
+}
+
+server.use(middlewares)
+server.use(router)
+server.listen(3000, () => {
+  console.log('JSON Server mock API is running')
+})
