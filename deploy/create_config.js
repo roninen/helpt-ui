d6803da8374ed69a95d46ecf9de7fe832d8c4ff4
Jussi Arpalahti <jussi.arpalahti@hel.fi>
2018-02-15 23:12:43 +0200

    Npm script creating config file from environment

---
+++ deploy/create_config.js
@@ -0,0 +1,25 @@
+
+const fs = require('fs');
+
+function create() {
+  if (process.env.API_URL) {
+    const config = {
+      API_URL: process.env.API_URL,
+      // The SSO OpenId Connect client id
+      OPENID_CONNECT_CLIENT_ID: process.env.OPENID_CONNECT_CLIENT_ID,
+      // A test user uuid for open id connect
+      // if you wish to play with the mock API using
+      // a real login flow
+      TEST_USER_UUID: process.env.TEST_USER_UUID
+    };
+    fs.writeFileSync('dist/assets/config.js', `window.CONFIG = ${JSON.stringify(config)}`, {encoding: 'utf8'});
+    console.log("Environment configuration saved");
+    return 0;
+  } else {
+    console.log('CONFIGURATION ERROR');
+    console.log('configuration environmental variables missing');
+    return 1;
+  }
+}
+
+process.exit(create());
